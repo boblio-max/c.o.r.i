d90bc8e700a962fa79b9afaf1f5845b91f9c14eb
Nikhil Mahankali <nikhilmahankali56@gmail.com>
2026-03-10 13:49:14 -0700

    hand tracking

---
+++ hand_tracking/handtracking.py
@@ -0,0 +1,45 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=2,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        continue
+
+    image = cv2.flip(image, 1)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(image_rgb)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style()
+            )
+            print(hand_landmarks[8])
+
+    # Display the feed
+    cv2.imshow('Hand Tracking', image)
+    if cv2.waitKey(5) & 0xFF == 27: # Press 'ESC' to exit
+        break
+
+# Release resources
+hands.close()
+cap.release()
+cv2.destroyAllWindows()
